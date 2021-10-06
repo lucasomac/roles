98acfd422f644b9a97c678eecea5104d7777eef0
Lucas de Oliveira Macedo <lucasomac@santander.com.br>
2021-10-05 22:38:35 -0300

    Updated: color to box decoration

--- lib/src/screens/list_roles.dart
+++ lib/src/screens/list_roles.dart
@@ -10,48 +10,45 @@ class ListRoles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Roles",
-      theme: ThemeData(
-        // Define the default brightness and colors.
-        brightness: Brightness.light,
-        primaryColor: Colors.redAccent,
-        appBarTheme: const AppBarTheme(color: Colors.redAccent),
-        // Define the default font family.
-        fontFamily: 'Roboto',
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Roles"),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Roles"),
-        ),
-        body: StreamBuilder<QuerySnapshot>(
-            stream: getAllRoles(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return const Text('Something went wrong');
-              }
+      body: StreamBuilder<QuerySnapshot>(
+          stream: getAllRoles(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
 
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Text("Loading");
-              }
-              return GridView.count(
-                crossAxisCount: 2,
-                childAspectRatio: 1.0,
-                padding: const EdgeInsets.all(8.0),
-                mainAxisSpacing: 8.0,
-                crossAxisSpacing: 8.0,
-                children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                  Role data =
-                      Role.fromJson(document.data()! as Map<String, dynamic>);
-                  return GestureDetector(
-                    onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ItemRole(data))),
-                    child: ItemMiniature(data),
-                  );
-                }).toList(),
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: LinearProgressIndicator(),
               );
-            }),
+            }
+            return GridView.count(
+              crossAxisCount: 2,
+              childAspectRatio: 1.0,
+              padding: const EdgeInsets.all(8.0),
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Role data =
+                    Role.fromJson(document.data()! as Map<String, dynamic>);
+                return GestureDetector(
+                  onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ItemRole(data))),
+                  child: ItemMiniature(data),
+                );
+              }).toList(),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+        elevation: 16,
       ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
     );
   }
 }
