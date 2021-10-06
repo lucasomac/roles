da3ae828e703c8a47124f18a7b4121a144352050
Lucas de Oliveira Macedo <lucasomac@santander.com.br>
2021-10-05 22:40:03 -0300

    Updated: themeData for app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:roles/src/screens/list_roles.dart';
+import 'package:roles/src/util/colors_pallet.dart';
 
 /// The Widget that configures your application.
 class App extends StatelessWidget {
@@ -22,8 +23,36 @@ class App extends StatelessWidget {
       // Define a light and dark color theme. Then, read the user's
       // preferred ThemeMode (light, dark, or system default) from the
       // SettingsController to display the correct theme.
-      theme: ThemeData(),
-      darkTheme: ThemeData.dark(),
+
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primaryColor: ColorsPallet.primary,
+        accentColor: ColorsPallet.secondary,
+        primaryColorLight: ColorsPallet.primaryLight,
+        primaryColorDark: ColorsPallet.primaryDark,
+        textTheme: const TextTheme(
+            headline1: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 48,
+                color: ColorsPallet.textPrimary),
+            subtitle1: TextStyle(
+                fontSize: 24,
+                fontStyle: FontStyle.italic,
+                color: ColorsPallet.textSecondary),
+            bodyText1: TextStyle(
+              fontSize: 14,
+              color: ColorsPallet.textSecondary,
+            ),
+            subtitle2: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: ColorsPallet.primary)),
+        appBarTheme: const AppBarTheme(color: ColorsPallet.primary),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: ColorsPallet.secondary,
+          foregroundColor: ColorsPallet.textSecondary,
+        ),
+      ),
       title: 'Roles',
       home: const ListRoles(),
     );
